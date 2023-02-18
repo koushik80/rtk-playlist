407bc0feb0e3aa950af14943f48428c07676ff2f
Koushik Dey <koushik.dey@live.com>
2023-02-18 02:36:57 +0200

    reset movie list

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,9 @@ const moviesSlice = createSlice({
     removeMovie(state, action) {
       const index = state.indexOf(action.payload);
       state.splice(index, 1);
+    },
+    reset(state, action) {
+      return [];
     }
   }
 });
@@ -37,4 +40,4 @@ const store = configureStore({
 
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
-export const { addMovie, removeMovie } = moviesSlice.actions;
+export const { addMovie, removeMovie, reset } = moviesSlice.actions;
